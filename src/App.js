df09fb4d8041e994c42a28300a3971de40b0a90c
Gaurav Bhandarkar <gauravbhandarkar94@gmail.com>
2021-09-05 00:01:52 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+const api = {
+  key: "686b26bd01aaf6d6beeda3a45a4ec1eb",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }

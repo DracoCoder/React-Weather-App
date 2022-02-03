c3c32266a6faf17c830ce8bffa4826609524a3e1
DracoCoder <gauravbhandarkar94@gmail.com>
2022-02-03 15:36:25 +0530

    Improved scaling on bigger displays and some minor twicks

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
           <input
             type="text"
             className="search-bar"
-            placeholder="City.."
+            placeholder="City, State, Country.."
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}

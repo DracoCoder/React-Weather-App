c1f058a4854cd0c1797dc623e3338929bbb0bf0e
DracoCoder <gauravbhandarkar94@gmail.com>
2022-02-06 21:49:54 +0530

    Initial Commit For Weather Icon

--- src/App.js
+++ src/App.js
@@ -58,6 +58,9 @@ function App() {
               <div className="weather-box">
                 <div className="temp">{Math.round(weather.main.temp)}°C</div>
                 <div className="weather">{weather.weather[0].main}</div>
+                <div className="weathericon">
+                  <img src="https://www.svgrepo.com/show/57154/fog.svg" alt="fog" height="90px" width="120px"></img>
+                </div>
               </div>
             </div>
           </div>
